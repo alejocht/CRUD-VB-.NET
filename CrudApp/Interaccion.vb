aee0de11af68bd250aa68f58d95b233b3711c9a4
Alejo Chavez <alejo.chavez@alumnos.frgp.utn.edu.ar>
2024-09-09 12:48:17 -0300

    Interacciones de consola

--- CrudApp/Interaccion.vb
+++ CrudApp/Interaccion.vb
@@ -10,11 +10,11 @@ Public Class Interaccion
             For Each producto In lista
                 Console.WriteLine(producto.ToString())
             Next
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
         Catch ex As Exception
             Console.WriteLine("Hubo un error al mostrar la lista:" + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
@@ -33,12 +33,12 @@ Public Class Interaccion
             Console.Clear()
             Console.WriteLine("El producto se agrego correctamente.")
             Console.WriteLine(aux.ToString())
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         Catch ex As Exception
             Console.WriteLine("Hubo un error al agregar el producto: " + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
@@ -57,13 +57,13 @@ Public Class Interaccion
             aux.categoria = InputBox("Categoria Actual: " + aux.categoria + "   Nueva Categoria: ")
             productoNegocio.modificar(aux)
             Console.WriteLine("Modificacion Exitosa.")
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
 
         Catch ex As Exception
             Console.WriteLine("Hubo un error al modificar el producto: " + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
@@ -74,14 +74,16 @@ Public Class Interaccion
             Dim clienteNegocio As New ClienteNegocio
             Dim lista As New List(Of Cliente)
             lista = clienteNegocio.listar()
+            'Console.WriteLine($"{"ID".PadRight(10)} {"Cliente".PadRight(50)} {"Telefono".PadRight(20)} {"Correo".PadRight(20)}")
+            'Console.WriteLine(New String("-"c, 100))
             For Each cliente In lista
                 Console.WriteLine(cliente.ToString())
             Next
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
         Catch ex As Exception
             Console.WriteLine("Hubo un error al mostrar la lista:" + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
@@ -99,12 +101,12 @@ Public Class Interaccion
             Console.Clear()
             Console.WriteLine("El cliente se agrego correctamente.")
             Console.WriteLine(aux.ToString())
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         Catch ex As Exception
             Console.WriteLine("Hubo un error al agregar el cliente: " + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
@@ -123,13 +125,13 @@ Public Class Interaccion
             aux.correo = InputBox("Categoria Actual: " + aux.correo + "   Nueva Categoria: ")
             clienteNegocio.modificar(aux)
             Console.WriteLine("Modificacion Exitosa.")
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
 
         Catch ex As Exception
             Console.WriteLine("Hubo un error al modificar el producto: " + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
@@ -143,11 +145,11 @@ Public Class Interaccion
             For Each venta In lista
                 Console.WriteLine(venta.ToString())
             Next
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
         Catch ex As Exception
             Console.WriteLine("Hubo un error al mostrar la lista:" + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
@@ -165,12 +167,12 @@ Public Class Interaccion
             Console.Clear()
             Console.WriteLine("La venta se agrego correctamente.")
             Console.WriteLine(aux.ToString())
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         Catch ex As Exception
             Console.WriteLine("Hubo un error al agregar la venta: " + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
@@ -189,12 +191,12 @@ Public Class Interaccion
             aux.total = InputBox("Total Actual: " + aux.total + "   Nuevo total: ")
             negocio.modificar(aux)
             Console.WriteLine("Modificacion Exitosa.")
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         Catch ex As Exception
             Console.WriteLine("Hubo un error al modificar la venta: " + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
@@ -208,35 +210,150 @@ Public Class Interaccion
             For Each ventaItem In lista
                 Console.WriteLine(ventaItem.ToString())
             Next
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
         Catch ex As Exception
             Console.WriteLine("Hubo un error al mostrar la lista:" + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
     End Sub
 
-    Public Sub agregarVenta()
+    Public Sub agregarVentaItem()
         Try
             Dim aux As New VentaItem
             Dim negocio As New VentaItemNegocio
+            Dim producto As New Producto
+            Dim negocioProducto As New ProductoNegocio
             Console.WriteLine("CREAR NUEVO VENTA ITEM:")
-            aux.idVenta = InputBox("Fecha yyyy-mm-dd : ")
-            aux.idProducto = InputBox("Total: ")
+            aux.idVenta = InputBox("ID de Venta: ")
+            aux.idProducto = InputBox("ID de Producto: ")
+            producto = negocioProducto.listar(aux.idProducto)
+            aux.precioUnitario = producto.precio
+            aux.cantidad = InputBox("Cantidad: ")
+            aux.precioTotal = aux.precioUnitario * aux.cantidad
             negocio.agregar(aux)
             Console.Clear()
-            Console.WriteLine("La venta se agrego correctamente.")
+            Console.WriteLine("El item de venta se agrego correctamente.")
             Console.WriteLine(aux.ToString())
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         Catch ex As Exception
             Console.WriteLine("Hubo un error al agregar la venta: " + ex.Message)
-            Console.WriteLine("Toca una tecla para continuar")
+            Console.WriteLine("Toca una tecla para continuar...")
             Console.ReadKey()
             Console.Clear()
         End Try
     End Sub
+
+    Public Sub modificarVentaItem()
+        Try
+            Dim aux As New VentaItem
+            Dim negocio As New VentaItemNegocio
+            Dim idBuscar As Integer
+            idBuscar = InputBox("Ingrese el ID del item de venta a modificar: ")
+            aux = negocio.listar(idBuscar)
+            Console.Clear()
+            aux.idVenta = InputBox("ID Venta Actual: " + aux.idVenta.ToString() + "  Nuevo ID Venta: ")
+            aux.idProducto = InputBox("ID Producto Actual: " + aux.idProducto.ToString() + "  Nuevo ID Producto: ")
+            aux.cantidad = InputBox("Cantidad Actual: " + aux.cantidad + "   Nueva cantidad: ")
+            negocio.modificar(aux)
+            Console.WriteLine("Modificacion Exitosa.")
+            Console.WriteLine("Toca una tecla para continuar...")
+            Console.ReadKey()
+            Console.Clear()
+        Catch ex As Exception
+            Console.WriteLine("Hubo un error al modificar el item de venta: " + ex.Message)
+            Console.WriteLine("Toca una tecla para continuar...")
+            Console.ReadKey()
+            Console.Clear()
+        End Try
+    End Sub
+
+    Public Sub mostrarMenu()
+        Console.WriteLine("-MENU-")
+        Console.WriteLine("1. Clientes")
+        Console.WriteLine("2. Productos")
+        Console.WriteLine("3. Ventas")
+        Console.WriteLine("0. Salir")
+        Console.WriteLine("Presione el numero que desee...")
+    End Sub
+
+    Public Sub mostrarSubMenuClientes()
+        Console.WriteLine("-MENU CLIENTES-")
+        Console.WriteLine("1. Listar")
+        Console.WriteLine("2. Agregar")
+        Console.WriteLine("3. Modificar")
+        Console.WriteLine("4. Filtrar")
+        Console.WriteLine("0. Volver")
+        Console.WriteLine("Presione el numero que desee...")
+        Dim consola As New Interaccion
+        Dim key As ConsoleKeyInfo = Console.ReadKey()
+        Console.Clear()
+        Select Case key.KeyChar
+            Case "1"
+                consola.mostrarClientes()
+            Case "2"
+                consola.agregarCliente()
+            Case "3"
+                consola.modificarCliente()
+            Case "4"
+                'falta desarrollar
+            Case "0"
+                Exit Sub
+        End Select
+    End Sub
+
+    Public Sub mostrarSubMenuProductos()
+        Console.WriteLine("-MENU PRODUCTOS-")
+        Console.WriteLine("1. Listar")
+        Console.WriteLine("2. Agregar")
+        Console.WriteLine("3. Modificar")
+        Console.WriteLine("4. Filtrar")
+        Console.WriteLine("0. Volver")
+        Console.WriteLine("Presione el numero que desee...")
+        Dim consola As New Interaccion
+        Dim key As ConsoleKeyInfo = Console.ReadKey()
+        Console.Clear()
+        Select Case key.KeyChar
+            Case "1"
+                consola.mostrarProductos()
+            Case "2"
+                consola.agregarProducto()
+            Case "3"
+                consola.modificarProducto()
+            Case "4"
+                'falta desarrollar
+            Case "0"
+                Exit Sub
+        End Select
+    End Sub
+
+    Public Sub mostrarSubMenuVentas()
+        Console.WriteLine("-MENU VENTAS-")
+        Console.WriteLine("1. Listar")
+        Console.WriteLine("2. Agregar")
+        Console.WriteLine("3. Modificar")
+        Console.WriteLine("4. Filtrar")
+        Console.WriteLine("0. Volver")
+        Console.WriteLine("Presione el numero que desee...")
+        Dim consola As New Interaccion
+        Dim key As ConsoleKeyInfo = Console.ReadKey()
+        Console.Clear()
+        Select Case key.KeyChar
+            Case "1"
+                consola.mostrarVentas()
+            Case "2"
+                consola.agregarVenta()
+            Case "3"
+                consola.modificarVenta()
+            Case "4"
+                'falta desarrollar
+            Case "0"
+                Exit Sub
+        End Select
+    End Sub
+
 End Class
--- CrudApp/Principal.vb
+++ CrudApp/Principal.vb
@@ -1,16 +1,31 @@
-﻿Imports modelo.modelo
+﻿Imports System.Linq.Expressions
+Imports modelo.modelo
 Imports negocio
 
 Module Principal
 
     Sub Main()
-        'Dim producto As New Producto(1, "Taza", 300, "Hogar")
-        'productoNegocio.agregar(producto)
-        Dim interaccion As New Interaccion
-
-        'interaccion.agregarCliente()
-        interaccion.mostrarClientes()
-        interaccion.modificarCliente()
+        Dim finDelPrograma As Boolean = False
+        While Not (finDelPrograma)
+            Dim consola As New Interaccion
+            consola.mostrarMenu()
+            Dim key As ConsoleKeyInfo = Console.ReadKey()
+            Console.Clear()
+            Select Case key.KeyChar
+                Case "1"
+                    consola.mostrarSubMenuClientes()
+                Case "2"
+                    consola.mostrarSubMenuProductos()
+                Case "3"
+                    consola.mostrarSubMenuVentas()
+                Case "0"
+                    Console.WriteLine($"{"ID".PadRight(10)} {"Cliente".PadRight(50)} {"Telefono".PadRight(20)} {"Correo".PadRight(20)}")
+                    Console.WriteLine(New String("-"c, 100))
+                    Console.ReadKey()
+                    finDelPrograma = True
+            End Select
+            Console.Clear()
+        End While
 
     End Sub
 
